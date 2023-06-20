5a3fd3479aae3c3a56ab9637afc3d70e4ab0412b
Pallav Jyoti Buragohain <pallav.assam@gmail.com>
2023-06-20 12:10:49 +0530

    initial commit

--- Default.aspx
+++ Default.aspx
@@ -3,58 +3,25 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
     <main>
-        <%-- <section class="row" aria-labelledby="aspnetTitle">
-            <h1 id="aspnetTitle">ASP.NET</h1>
-            <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-            <p><a href="http://www.asp.net" class="btn btn-primary btn-md">Learn more &raquo;</a></p>
-        </section> --%>
-
-        <%-- <div class="row">
-            <section class="col-md-4" aria-labelledby="gettingStartedTitle">
-                <h2 id="gettingStartedTitle">Getting started</h2>
-                <p>
-                    ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-                A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-                </p>
-                <p>
-                    <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-                </p>
-            </section>
-            <section class="col-md-4" aria-labelledby="librariesTitle">
-                <h2 id="librariesTitle">Get more libraries</h2>
-                <p>
-                    NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-                </p>
-                <p>
-                    <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-                </p>
-            </section>
-            <section class="col-md-4" aria-labelledby="hostingTitle">
-                <h2 id="hostingTitle">Web Hosting</h2>
-                <p>
-                    You can easily find a web hosting company that offers the right mix of features and price for your applications.
-                </p>
-                <p>
-                    <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-                </p>
-            </section>
-        </div> --%>
+        
 
         <div class="container w-50 border border-1 rounded rounded-3 p-5">
         <h1>Lease Form</h1>
         <hr/>
-        <form class="mt-4">
+        <form class="mt-4" id="Form_submit" runat="server">
             <div class="mb-3">
                 <label for="id" class="form-label">ID</label>
-                <input type="number" class="form-control" id="id" placeholder="Enter id">
+                <asp:Textbox TextMode="Number" class="form-control" ID="id1" runat="server" placeholder="Enter id"></asp:Textbox>
               </div>
+
               <div class="mb-3">
-                <label for="lease-id" class="form-label">Lease ID</label>
-                <input type="text" class="form-control" id="lease-id" placeholder="Lease ID">
+                <label for="lease_id" class="form-label">Lease ID</label>
+                <input type="text" class="form-control" id="lease_id" placeholder="Lease ID" runat="server" >
               </div>
+
               <div class="mb-3">
-                <label for="lease-id" class="form-label">BLOCK_TYPE</label>
-                <select class="form-select">
+                <label for="block_type" class="form-label">BLOCK_TYPE</label>
+                <select class="form-select"  runat="server" >
                     <option value="1">REL</option>
                     <option value="2">PEL_OLD</option>
                     <option value="3">ML</option>
@@ -67,44 +34,71 @@
                     <option value="10">DSF</option>
                   </select>
               </div>
+
               <div class="mb-3">
-                <label for="NELP_ROUND" class="form-label">NELP_ROUND</label>
-                <input type="text" class="form-control" name="NELP_ROUND" id="NELP_ROUND" placeholder="NELP_ROUND">
-              </div>
-              <div class="mb-3">
-                <label for="NELP_ROUND" class="form-label">MINISTRY_REFERENCE_NO</label>
-                <input type="text" class="form-control" name="MINISTRY_REFERENCE_NO" id="MINISTRY_REFERENCE_NO" placeholder="MINISTRY_REFERENCE_NO">
-              </div>  <div class="mb-3">
-                <label for="NELP_ROUND" class="form-label">LEASE_NUMBER</label>
-                <input type="text" class="form-control" name="LEASE_NUMBER" id="LEASE_NUMBER" placeholder="LEASE_NUMBER">
-              </div>
-              <div class="mb-3">
-                <label for="NELP_ROUND" class="form-label">MINISTRY_REFERENCE_NO</label>
-                <input type="text" class="form-control" name="MINISTRY_REFERENCE_NO" id="MINISTRY_REFERENCE_NO" placeholder="MINISTRY_REFERENCE_NO">
+                <label for="nelp_round" class="form-label">NELP_ROUND</label>
+                <input type="text" class="form-control" name="NELP_ROUND" id="nelp_round" placeholder="NELP_ROUND"  runat="server" >
               </div>
 
               <div class="mb-3">
-                <label for="EFFECTIVE_DATE" class="form-label">EFFECTIVE_DATE</label>
-                <input type="date" class="form-control" name="EFFECTIVE_DATE" id="EFFECTIVE_DATE" placeholder="Lease ID">
+                <label for="min_refno" class="form-label">MINISTRY_REFERENCE_NO</label>
+                <input type="text" class="form-control" name="MINISTRY_REFERENCE_NO" id="min_refno" placeholder="MINISTRY_REFERENCE_NO" runat="server" >
+              </div>  
+            
+            <div class="mb-3">
+                <label for="lease_no" class="form-label">LEASE_NUMBER</label>
+                <input type="text" class="form-control" name="LEASE_NUMBER" id="lease_no" placeholder="LEASE_NUMBER" runat="server" >
               </div>
+
+            <div class="mb-3">
+                <label for="state_name" class="form-label">STATE_NAME</label>
+                <select id="States" class="form-select" runat="server">
+                    <option>Select</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="project" class="form-label">PROJECT</label>
+                <select id="Projects" class="form-select" runat="server">
+                    <option>Select</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="block_name" class="form-label">BLOCK_NAME</label>
+                <select id="Block" class="form-select" runat="server">
+                    <option>Select</option>
+                </select>
+            </div>
+
               <div class="mb-3">
-                <label for="EXT_YEAR" class="form-label">EXT_YEAR</label>
-                <input type="number" class="form-control" name="EXT_YEAR" id="EXT_YEAR" placeholder="2015" min="1990" step="1">
+                <label for="eff_date" class="form-label">EFFECTIVE_DATE</label>
+                <input type="date" class="form-control" name="EFFECTIVE_DATE" id="eff_date" placeholder="Lease ID" runat="server" >
               </div>
+
               <div class="mb-3">
-                <label for="Remark" class="form-label">Remarks</label>
-                <textarea class="form-control" id="Remark" rows="3"></textarea>
+                <label for="ext_year" class="form-label">EXT_YEAR</label>
+                <input type="number" class="form-control" name="EXT_YEAR" id="ext_year" placeholder="2015" min="1990" step="1" runat="server" >
               </div>
+
               <div class="mb-3">
-                <label for="AREA_ID" class="form-label">AREA_ID</label>
-                <input type="number" class="form-control" id="id" placeholder="110">
+                <label for="remark" class="form-label">Remarks</label>
+                <textarea class="form-control" id="remark" rows="3" runat="server" ></textarea>
+              </div>
+
+              <div class="mb-3">
+                <label for="area_id" class="form-label">AREA_ID</label>
+                <input type="number" class="form-control" id="area_id" placeholder="110" runat="server" >
               </div>
 
 
-              <button class="btn btn-primary ">Submit</button>
+             
+              <button class="btn btn-primary" onclick="SubmitForm_Click"  runat="server" >Submit</button>
+
         </form>
     </div>
-
+    <%-- <div id="prjOutput" runat="server">
+    </div> --%>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </main>
 
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OracleClient;
+//using Oracle.ManagedDataAccess.Client;
 
 namespace DataLoader
 {
@@ -11,7 +13,184 @@ namespace DataLoader
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string myConnectionString = "Data Source = orcl; User Id = records; Password = records;";
+            OracleConnection myConnection = new OracleConnection();
+            myConnection.ConnectionString = myConnectionString;
+            /*try
+            {
+                id.Value = "111111";
+                myConnection.Open();
+                Console.WriteLine("Connection successful!");
+                *//*String sql = "SELECT * FROM W_R_PROJECT_";*//*
+                String op = "";
 
+
+                using (OracleCommand command = new OracleCommand(sql, myConnection))
+                {
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Access the columns using the reader's GetXXX methods
+
+                            string name = reader.GetString(2); // Assuming the second column is of type VARCHAR2
+                            string code = reader.GetString(6); // Assuming the second column is of type VARCHAR2
+
+                            op = op + ($"Name: {name}, Code: {code}") + "<br>";
+
+                        }
+                    }
+                }
+
+                
+                Console.WriteLine(op);
+                // Perform database operations here
+                *//*prjOutput.InnerHtml = op;*//*
+                myConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }*/
+
+
+            try
+            {
+                myConnection.Open();
+                string state_sql = "SELECT * FROM W_R_STATE_";
+                using (OracleCommand command = new OracleCommand(state_sql, myConnection))
+                {
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Access the columns using the reader's GetXXX methods
+                            string name = reader.GetString(2); // Assuming the second column is of type VARCHAR2
+                            // Assuming the second column is of type VARCHAR2
+                            States.Items.Add(name);
+                        }
+                    }
+                }
+                myConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                myConnection.Open();
+                string proj_sql = "SELECT * FROM W_R_PROJECT_";
+                using (OracleCommand command = new OracleCommand(proj_sql, myConnection))
+                {
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string name = reader.GetString(2);
+                            Projects.Items.Add(name);
+                        }
+                    }
+                }
+                myConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+
+            try
+            {
+                myConnection.Open();
+                string block_sql = "SELECT * FROM W_BASIN_";
+                using (OracleCommand command = new OracleCommand(block_sql, myConnection))
+                {
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string name = reader.GetString(10); 
+                            Block.Items.Add(name);
+                        }
+                    }
+                }
+                myConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+
+
+        protected void SubmitForm_Click(object sender, EventArgs e)
+        {
+            string myConnectionString = "Data Source=orcl; User Id=records; Password=records;";
+            OracleConnection myConnection = new OracleConnection(myConnectionString);
+
+            try
+            {
+                myConnection.Open();
+
+                // Retrieve form values
+                int id1 = int.Parse(this.id1.Text);
+                string leaseId = lease_id.Value;
+                string blockType = Request.Form["block_type"];
+                string nelpRound = nelp_round.Value;
+                string ministryReferenceNo = min_refno.Value;
+                string leaseNumber = lease_no.Value;
+                /*string stateName = States.SelectedItem.Text;
+                string projectName = Projects.SelectedItem.Text;
+                string blockName = Block.SelectedItem.Text;*/
+                string stateName = Request.Form["States"];
+                string projectName = Request.Form["Projects"];
+                string blockName = Request.Form["Block"];
+                string effectiveDate = eff_date.Value;
+                int extYear = int.Parse(ext_year.Value);
+                string remarks = remark.Value;
+                int areaId = int.Parse(area_id.Value);
+
+                // Prepare SQL insert statement
+                /*string insertSql = "INSERT INTO W_LEASE_ (ID, LEASE_ID, BLOCK_TYPE, NELP_ROUND, MINISTRY_REFERENCE_NO, " +
+                    "LEASE_NUMBER, STATE_NAME, PROJECT, BLOCK_NAME, EFFECTIVE_DATE, EXT_YEAR, REMARKS, " +
+                    "INSERT_DATE, INSERT_USER, UPDATE_DATE, UPDATE_USER, AREA_ID) " +
+                    "VALUES (@id, @leaseId, @blockType, @nelpRound, @ministryReferenceNo, @leaseNumber, @stateName, " +
+                    "@projectName, @blockName, TO_DATE(@effectiveDate, 'YYYY-MM-DD'), @extYear, @remarks, " +
+                    "SYSDATE, 'admin', SYSDATE, 'admin', @areaId)";*/
+
+                /*string insertSql = "INSERT INTO W_LEASE_ (ID, LEASE_ID, BLOCK_TYPE, NELP_ROUND, MINISTRY_REFERENCE_NO, LEASE_NUMBER, STATE_NAME, PROJECT, BLOCK_NAME, EFFECTIVE_DATE, EXT_YEAR, REMARKS, INSERT_DATE, INSERT_USER, UPDATE_DATE, UPDATE_USER, AREA_ID) VALUES (@id, ";*/
+
+                string insertSql = "INSERT INTO W_LEASE_ (ID, LEASE_ID, BLOCK_TYPE, NELP_ROUND, MINISTRY_REFERENCE_NO, LEASE_NUMBER, STATE_NAME, PROJECT, BLOCK_NAME, EFFECTIVE_DATE, EXT_YEAR, REMARKS, INSERT_DATE, INSERT_USER, UPDATE_DATE, UPDATE_USER, AREA_ID) VALUES (id1, leaseId, blockType, nelpRound, ministryReferenceNo, leaseNumber, stateName,projectName, blockName, TO_DATE(effectiveDate, 'YYYY-MM-DD'), extYear, remarks, SYSDATE, 'admin', SYSDATE, 'admin', areaId); ";
+                using (OracleCommand command = new OracleCommand(insertSql, myConnection))
+                {
+                    // Bind parameters
+                    /*command.Parameters.Add("@id", OracleType.Number).Value = id;
+                    command.Parameters.Add("@leaseId", OracleType.VarChar).Value = leaseId;
+                    command.Parameters.Add("@blockType", OracleType.VarChar).Value = blockType;
+                    command.Parameters.Add("@nelpRound", OracleType.VarChar).Value = nelpRound;
+                    command.Parameters.Add("@ministryReferenceNo", OracleType.VarChar).Value = ministryReferenceNo;
+                    command.Parameters.Add("@leaseNumber", OracleType.VarChar).Value = leaseNumber;
+                    command.Parameters.Add("@stateName", OracleType.VarChar).Value = stateName;
+                    command.Parameters.Add("@projectName", OracleType.VarChar).Value = projectName;
+                    command.Parameters.Add("@blockName", OracleType.VarChar).Value = blockName;
+                    command.Parameters.Add("@effectiveDate", OracleType.DateTime).Value = effectiveDate;
+                    command.Parameters.Add("@extYear", OracleType.Number).Value = extYear;
+                    command.Parameters.Add("@remarks", OracleType.VarChar).Value = remarks;
+                    command.Parameters.Add("@areaId", OracleType.Number).Value = areaId;*/
+
+                    // Execute the SQL statement
+                    command.ExecuteNonQuery();
+                }
+
+                myConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
     }
 }
